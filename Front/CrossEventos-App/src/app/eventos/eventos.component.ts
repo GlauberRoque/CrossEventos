f0f66c4c2c090b1569f7175e0d73799d273a6e6e
GlauberRoque <80328841+GlauberRoque@users.noreply.github.com>
2023-05-09 13:13:23 -0300

    Atualizações angular Eventos

--- Front/CrossEventos-App/src/app/eventos/eventos.component.ts
+++ Front/CrossEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit{
 
-public eventos: any;
+public eventos: any = [];
+public eventosFiltrados: any = [];
+
+larguraImagem = 100;
+margemImagem = 2;
+exibirImagem: boolean = true;
+private _filtroLista: string = '';
+
+public get filtroLista(): string {
+  return this._filtroLista;
+}
+public set filtroLista(value: string){
+  this._filtroLista = value;
+  this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+}
+//  metodo para filtrar os eventos pelo nome e local
+filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { nome: string; local: string; }) => evento.nome.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+}
 
 constructor(private http: HttpClient) {}
 
@@ -17,12 +39,19 @@ ngOnInit(): void {
 
 }
 
+alterarImagem() {
+  this.exibirImagem = !this.exibirImagem;
+}
+
   public getEventos(): void {
-    this.http.get('https://localhost:7023/api/Evento').subscribe(
-      response => this.eventos = response,
+      this.http.get('https://localhost:7023/api/Evento').subscribe(
+      response => {
+         this.eventos = response
+         this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
 
-    );
+   );
 
   }
 
