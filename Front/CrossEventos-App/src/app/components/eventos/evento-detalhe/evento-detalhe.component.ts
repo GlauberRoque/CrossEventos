1cd8d4d607ef7e036b0b149c9994569ef5be5846
GlauberRoque <80328841+GlauberRoque@users.noreply.github.com>
2023-05-11 04:30:14 -0300

    Finalizando forms reativos

--- Front/CrossEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/CrossEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
   templateUrl: './evento-detalhe.component.html',
   styleUrls: ['./evento-detalhe.component.css']
 })
-export class EventoDetalheComponent {
+export class EventoDetalheComponent implements OnInit {
+
+  form!: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  public validation(): void{
+    this.form = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]],
+      local: ['', Validators.required],
+      dataEvento: ['', Validators.required],
+      qtdPessoas: ['', Validators.required],
+      telefone: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      imagemURL: ['', Validators.required]
+    });
+  }
+
+  resetForm(): void{
+    this.form.reset();
+  }
 
 }
--- Front/CrossEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/CrossEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from 'src/app/helpers/validatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  get f(): any {
+    return this.form.controls;
+  }
+
+  form!: FormGroup;
+
+  constructor(public fb: FormBuilder) { }
 
   ngOnInit() {
+    this.validation();
+  }
+
+  private validation(): void{
+    const formOptions: AbstractControlOptions ={
+      validators: ValidatorField.MustMatch('senha', 'confirmaSenha')
+    };
+
+      this.form = this.fb.group({
+        primeiroNome: ['', Validators.required],
+        ultimoNome: ['', Validators.required],
+        email: ['',[ Validators.required, Validators.email]],
+        telefone: ['', Validators.required],
+        funcao: ['', Validators.required],
+        descricao: ['', [Validators.required, Validators.maxLength(256)]],
+        senha: ['', [Validators.required, Validators.minLength(6)]],
+        confirmaSenha: ['', Validators.required],
+      }, formOptions)
+  }
+
+  public resetForm(event: any): void{
+    event.preventDefault();
+    this.form.reset();
   }
 
 }
